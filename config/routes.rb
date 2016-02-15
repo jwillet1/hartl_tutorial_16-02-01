a07c4b36190ef4d57afdd46e8321cf10e2994821
Joshua Willett <jwillet1@gmail.com>
2016-02-15 19:02:36 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  root 'static_pages#home'
+  root                'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'

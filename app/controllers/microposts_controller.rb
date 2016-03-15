2264d1681c9bdf06601cbce94c967d3d2ee3dac2
Joshua Willett <jwillet1@gmail.com>
2016-03-15 15:53:30 +0000

    fix issue with static pages controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:succcess] = "Micropost created!"
+      flash[:success] = "Micropost created!"
       redirect_to root_url
     else
       @feed_items = []
@@ -18,15 +18,15 @@ class MicropostsController < ApplicationController
     flash[:success] = "Micropost deleted"
     redirect_to request.referrer || root_url
   end
-  
+
   private
-    
+
     def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+      params.require(:micropost).permit(:content)
     end
-    
+
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micrroppost.nil?
+      redirect_to root_url if @micropost.nil?
     end
 end
